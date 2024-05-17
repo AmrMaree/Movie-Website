b066bb81e0175f68c6b2e884434997b1115adaa3
AmrMaree <113312326+AmrMaree@users.noreply.github.com>
2024-05-17 11:02:53 +0300

    changes in movies page

--- MoviePage.js
+++ MoviePage.js
@@ -15,3 +15,27 @@ left_scroll.addEventListener('click', () => {
 right_scroll.addEventListener('click', () => {
     cato_bx.scrollLeft += 100;
 })
+
+
+let mvoes_bx_1 = document.getElementById('mvoes_bx_1');
+let left_scroll1 = document.getElementById('left_scroll1');
+let right_scroll1 = document.getElementById('right_scroll1');
+
+left_scroll1.addEventListener('click', () => {
+    mvoes_bx_1.scrollLeft -= 150;
+})
+right_scroll1.addEventListener('click', () => {
+    mvoes_bx_1.scrollLeft += 150;
+})
+
+
+   // year and a-z box start 
+let year = document.getElementById('year');
+let a_z = document.getElementById('a_z');
+
+year.addEventListener('click', () => {
+    year.classList.toggle('show_year1')
+});
+a_z.addEventListener('click', () => {
+    a_z.classList.toggle('show_year1')
+});
